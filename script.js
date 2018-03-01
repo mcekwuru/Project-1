854bff1e9c0e272f81eb39b51a63a6b4b3035e8a
Miriam <mcekwuru@yahoo.com>
2018-02-28 21:57:28 -0500

    patch #10

--- script.js
+++ script.js
@@ -1,6 +1,5 @@
-var scart = new shoppingcart();
-		scart.PayPal.business = "sandbox@paypal.com";
-		scart.inventory.Add("Gele","African Dashiki","50","African Dashiki,"Item is brand new"
-		scart.inventory.Add("Jewelry","African Head Wrap","25","Item is brand new"
-		scart.inventory.Add("AFRICANBEADS","African Beads","10","Item is brand new"
-		scart.inventory.Add("AFRICANSHEABUTTER","African Shea Butter (Sumi-e)","12.95","Item is brand new"
+var product
+function Add to cart ("price + quanity"){
+	document.getElementbyclass("gallery_item").innerHTML = myfunction
+}
+$
