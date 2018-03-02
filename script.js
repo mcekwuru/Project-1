b79c181178a7b64cd6bf88a6f43da2621b2ebbb3
Miriam <mcekwuru@yahoo.com>
2018-03-02 01:17:31 -0500

    added java

--- script.js
+++ script.js
@@ -1,1 +1,16 @@
+function updateCart() {
+    document.getElementById("num-of-items").innerHTML = 0;
+}
+
+function addToCart() {
+    var x1 = parseInt(document.getElementById("item1").value);
+    var x2 = parseInt(document.getElementById("item2").value);
+    var x3 = parseInt(document.getElementById("item3").value);
+    var x4 = parseInt(document.getElementById("item4").value);
+    var x5 = parseInt(document.getElementById("item5").value);
+    var x6 = parseInt(document.getElementById("item6").value);    
+    
+    var totalItems = x1 + x2 + x3 + x4 + x5 + x6;
+    document.getElementById("num-of-items").innerHTML = totalItems;
+}
 
